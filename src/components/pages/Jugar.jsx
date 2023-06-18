e3125247859b257d7a6bf47005f9e26dbb58b621
Víctor Alonso <victoralonso987@gmail.com>
2023-06-18 22:03:15 +0200

    18º commit

--- src/components/pages/Jugar.jsx
+++ src/components/pages/Jugar.jsx
@@ -13,14 +13,15 @@ import linea from '/src/images/linea (1).png';
 
 function About() {
 
-  const [fase1, setfase1] = useState(localStorage.getItem("fase1"));
-  const [fase2, setfase2] = useState(localStorage.getItem("fase2"));
-  const [fase3, setfase3] = useState(localStorage.getItem("fase3"));
+  
 
 
 
   useEffect(() => {
-
+    
+    const [fase1, setfase1] = useState(localStorage.getItem("fase1"));
+    const [fase2, setfase2] = useState(localStorage.getItem("fase2"));
+    const [fase3, setfase3] = useState(localStorage.getItem("fase3"));
       /*
     localStorage.setItem("fase1", '0');
     localStorage.setItem("fase2", '0');
